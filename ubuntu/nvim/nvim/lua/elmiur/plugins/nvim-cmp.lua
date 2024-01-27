b04cd1af14e620eed032358adc6d5e378e982931
woomiz <vishnups.developer@gmail.com>
2024-01-28 01:04:47 +0530

    feat: modularizing plugins, first attempt

--- ubuntu/nvim/nvim/lua/elmiur/plugins/nvim-cmp.lua
+++ ubuntu/nvim/nvim/lua/elmiur/plugins/nvim-cmp.lua
@@ -1,60 +1,83 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-buffer", -- source for text in buffer
-		"hrsh7th/cmp-path", -- source for file system paths
-		"L3MON4D3/LuaSnip", -- snippet engine
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"rafamadriz/friendly-snippets", -- useful snippets
-		"onsails/lspkind.nvim", -- vs-code like pictograms
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-buffer", -- source for text in buffer
+			"hrsh7th/cmp-path", -- source for file system paths
+			"onsails/lspkind.nvim", -- vs-code like pictograms
+		},
+		opts = function()
+			local cmp = require("cmp")
+
+			local lspkind = require("lspkind")
+			return {
+				completion = {
+					completeopt = "menu,menuone,preview,noselect",
+				},
+
+				mapping = cmp.mapping.preset.insert({
+					["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+					["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+					["<C-e>"] = cmp.mapping.abort(), -- close completion window
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+				}),
+
+				-- sources for autocompletion
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" }, -- lsp for autocompletion
+					{ name = "path" }, -- file system paths
+					{
+						{ name = "buffer" }, -- text within current buffer
+					},
+				}),
+
+				-- configure lspkind for vs-code like pictograms in completion menu
+				formatting = {
+					format = lspkind.cmp_format({
+						maxwidth = 50,
+						ellipsis_char = "...",
+					}),
+				},
+			}
+		end,
+		config = function(_, opts)
+			require("cmp").setup(opts)
+		end,
 	},
-	config = function()
-		local cmp = require("cmp")
+	{
+		"L3MON4D3/LuaSnip", -- snippet engine
+		dependencies = {
+			{
+				"nvim-cmp", -- adding snippets to auto completion
+				dependencies = {
+					"saadparwaiz1/cmp_luasnip", -- for luasnip to work with nvim-cmp
+				},
+				opts = function(_, opts)
+					opts.snippet = { -- configure how nvim-cmp interacts with snippet engine
+						expand = function(args)
+							require("luasnip").lsp_expand(args.body)
+						end,
+					}
 
-		local luasnip = require("luasnip")
-
-		local lspkind = require("lspkind")
-
-		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-		require("luasnip.loaders.from_vscode").lazy_load()
-
-		cmp.setup({
-			completion = {
-				completeopt = "menu,menuone,preview,noselect",
-			},
-
-			snippet = { -- configure how nvim-cmp interacts with snippet engine
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
+					table.insert(opts.sources, { name = "luasnip" })
 				end,
 			},
+			{
+				"rafamadriz/friendly-snippets", -- useful snippets
 
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-			}),
-
-			-- sources for autocompletion
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" }, -- lsp for autocompletion
-				{ name = "luasnip" }, -- snippets
-				{ name = "buffer" }, -- text within current buffer
-				{ name = "path" }, -- file system paths
-			}),
-
-			-- configure lspkind for vs-code like pictograms in completion menu
-			formatting = {
-				format = lspkind.cmp_format({
-					maxwidth = 50,
-					ellipsis_char = "...",
-				}),
+				config = function()
+					-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
 			},
-		})
-	end,
+		},
+		config = function()
+			-- load custom snippets
+			require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/elmiur/snippets/" })
+		end,
+	},
 }
