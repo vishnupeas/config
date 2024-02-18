38a0de4344841283100e32d650b69f74934bd31b
woomiz <vishnups.developer@gmail.com>
2024-02-18 18:58:26 +0530

    feat: add some keymaps for gitsigns

--- ubuntu/nvim/nvim/lua/elmiur/plugins/gitsigns.lua
+++ ubuntu/nvim/nvim/lua/elmiur/plugins/gitsigns.lua
@@ -4,6 +4,23 @@ return {
 	opts = {
 		current_line_blame = true,
 	},
+	keys = {
+		{
+			"<leader>ghp",
+			"<CMD>Gitsigns preview_hunk_inline<CR>",
+			desc = "Gitsigns preview_hunk_inline",
+		},
+		{
+			"<leader>ghs",
+			"<CMD>Gitsigns stage_hunk<CR>",
+			desc = "Gitsigns stage_hunk",
+		},
+		{
+			"<leader>gbs",
+			"<CMD>Gitsigns stage_buffer<CR>",
+			desc = "Gitsigns stage_buffer",
+		},
+	},
 	config = function(_, opts)
 		local gitsigns = require("gitsigns")
 		gitsigns.setup(opts)
